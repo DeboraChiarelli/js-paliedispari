ad52bdfc656f7476ebb9f6f076043d16c4875450
DeboraChiarelli <deborachiarelli92@gmail.com>
2023-10-12 17:47:57 +0200

    Add- palindroma

--- app.js
+++ app.js
@@ -9,3 +9,22 @@ Consigli del giorno
 Scriviamo sempre in italiano i passaggi che vogliamo fare
 Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.*/
 
+
+//# Palindroma
+//- Chiedere all'utente di inserire una parola attraverso un prompt
+let userWord = prompt('Inserisci una parola');
+//- creare un array formato dalle lettere della parola dell'utente
+let wordsArray = userWord.split('');
+    //-creare un array reverse
+let arrayReverse = wordsArray.reverse();
+    //-creare una stringa dell'array utilizzando Join (concatena tutti gli elementi in un array e li restituisce come stringa)
+let arrayJoin = arrayReverse.join('');
+//- controllare la palindromia
+    //- SE la parola inserita dall'utente è identica a arrayJoin
+if (userWord === arrayJoin) { //- ALLORA stamperà che l aparola è palindroma
+    console.log('La parola' + userWord + 'è palindroma');
+} else {
+    console.log('La parola' + userWord + 'non è palindroma');
+}
+
+
