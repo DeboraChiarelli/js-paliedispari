e5158c42b4c6bd7dd43ca9de3a7cd54198fd33af
DeboraChiarelli <deborachiarelli92@gmail.com>
2023-10-12 23:11:41 +0200

    Add - somma numeri e condizioni di vittoria

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ Creare una funzione per capire se la parola inserita è palindroma
 Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il 
 computer (usando una funzione). 
-Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+Sommiamo i due numeri 
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) 
+Dichiariamo chi ha vinto.
 Consigli del giorno
 Scriviamo sempre in italiano i passaggi che vogliamo fare
 Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.*/
@@ -43,6 +45,38 @@ if (userWord === reverse) {
 
 //# Pari e Dispari
 //- Chiedere all'utente, con un prompt, di scegliere tra pari e dispari, inserendo un numero da 1 a 5
-let choose = prompt('Pari o Dispari?');
+let evenOdd = prompt('Pari o Dispari?');
+console.log(evenOdd);
 //- chiedere all'utente di scegliere un numero
-let numberUser = prompt('Inserisci un numero da 1 a 5');
+let userNumber = prompt('Inserisci un numero da 1 a 5');
+//- generare un numero random
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random) * (max - min + 1) + min;
+}
+const computerNumber = generateRandomNumber(1, 5);
+//- Sommare i due numeri 
+const sumNumbers = parseInt(userNumber) + computerNumber;
+console.log(sumNumbers);
+//- dichiarare funzione per numero pari
+function isEven(number) {
+    return number % 2 == 0; 
+}
+    //- SE la somma dello userNumber e del computerNumber è pari
+if (isEven(sumNumbers)) {
+    console.log('evenOdd.toLowerCase() = ' + evenOdd.toLowerCase());
+    // - e SE la scelta dell'utente è pari
+    if (evenOdd.toLowerCase() === 'pari') {
+        console.log('Hai vinto'); //- ALLORA ha vinto
+    } else if (evenOdd.toLowerCase() === 'dispari') { //- SE sceglie dispari
+        console.log('Hai perso'); //- ALLORA ha perso
+    }
+} else { //- SE la somma dello userNumber e del computerNumber è dispari
+    console.log('evendOdd.toLowerCase() = ' + evenOdd.toLowerCase())
+    //- e SE la scelta dello user è dispari
+    if (evenOdd.toLocaleLowerCase() === 'dispari') {
+        console.log('Hai vinto'); //- ALLORA ha vinto
+    } else if (evenOdd.toLocaleLowerCase() === 'pari') { //- SE sceglie pari
+        console.log('Hai perso'); //- ALLORA ha perso
+    }
+}
+
